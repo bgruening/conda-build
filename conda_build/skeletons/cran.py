204dd95d96d73f3bf445964ccaacb81ce6c71a0a
Björn Grüning <bjoern@gruenings.eu>
2017-04-29 13:32:03 +0200

    not needed anymore

--- conda_build/skeletons/cran.py
+++ conda_build/skeletons/cran.py
@@ -99,10 +99,6 @@ about:
 CRAN_BUILD_SH = """\
 #!/bin/bash
 
-# R refuses to build packages that mark themselves as Priority: Recommended
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-
 $R CMD INSTALL --build .
 
 # Add more build steps here, if they are necessary.
